882cc7fb0d4b5611799a20017f7e7a26ed886887
theyadev <gtheya@gmail.com>
2021-12-17 16:10:48 +0100

    not working

--- src/tests/filetest-out.py
+++ src/tests/filetest-out.py
@@ -5,3 +5,16 @@ z = 3
 def main():
     new = "Ouiiii"
     return new
+mydict = {"oui": "non","hello": False,"peut-etre": {"ui": 1}}
+x = 0
+def ui():
+    pass
+def F(string: str,nb: int):return {"ui": True    }
+lst = [
+    {"nom": "Theo"},
+    {"nom": "Theo"},
+{"nom": "Theo"    }
+]
+compr = [ i
+    for i in range(len(lst)    )
+]
--- src/tests/filetest.py
+++ src/tests/filetest.py
@@ -8,6 +8,46 @@ def main():
     """
         Bonjour je suis une docstring
     """
+    """
+        z
+    """
     new = "Ouiiii"
     # Comment
     return new
+
+mydict = {
+    "oui": "non",
+    "hello": False,
+    "peut-etre": {
+        "ui": 1
+    }
+}
+
+x = 0
+
+def ui():
+    pass
+
+def F(
+    string: str,
+    nb: int
+):
+    return {
+        "ui": True
+    }
+
+lst = [
+    {"nom": "Theo"},
+    {"nom": "Theo"},
+    {
+        "nom": "Theo"
+    }
+]
+
+compr = [ i
+    for i in range(
+        len(
+            lst
+        )
+    )
+]
